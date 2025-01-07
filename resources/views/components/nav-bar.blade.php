5b7765f8c996c260e10f7db7fb16b471bc6dbbef
Afonso <afonsocamacho62@gmail.com>
2025-01-07 16:02:16 +0000

    Update navigation links; rename 'Projects' to 'Projects Type' for consistency

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -7,7 +7,7 @@
     <a href="/" class="logo-link"><p class="logo-text">( AFONSO CAMACHO ) </p></a>
     <ul id="nav-links" class="menu">
         <li><a href="/about-me">About Me</a></li>
-        <li><a href="/projects">Projects</a></li>
+        <li><a href="/projects-type">Projects</a></li>
         <li><a href="/contact">Contact</a></li>
     </ul>
     
